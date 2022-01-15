7875422d487f9e713d123ebc7bd59bf98fd01f6f
Frank <2521647197@qq.com>
2022-01-15 16:47:05 +0800

    基本功能完成

--- src/Layout.vue
+++ src/Layout.vue
@@ -1,13 +1,43 @@
 <template>
-
+  <div class="layout" :class="layoutClass">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Layout"
+  name: "Layout",
+  data(){
+    return{
+      layoutClass:{
+        hasSider:false
+      }
+    }
+  },
+  mounted() {
+    console.log(this.$children);
+    this.$children.forEach((vm)=>{
+      console.log(vm.$options.name);
+      if(vm.$options.name === 'Sider'){
+        this.layoutClass.hasSider = true
+      }
+    })
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.layout {
+  flex-grow: 1;
+  height: 100vh;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+
+  &.hasSider{
+     flex-direction: row;
+   }
+}
+
 
 </style>
--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from "./Button-group";
 import Input from "./Input";
 import Row from "./Row";
 import Col from "./Col";
+import Layout from "./Layout";
+import Header from "./Header";
+import Sider from "./Sider";
+import Content from "./Content";
+import Footer from "./Footer";
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
@@ -12,6 +17,12 @@ Vue.component('g-button-group',ButtonGroup)
 Vue.component('g-input',Input)
 Vue.component('g-row',Row)
 Vue.component('g-col',Col)
+Vue.component('g-layout',Layout)
+Vue.component('g-header',Header)
+Vue.component('g-sider',Sider)
+Vue.component('g-content',Content)
+Vue.component('g-footer',Footer)
+
 
 new Vue({
     el:'#app',
